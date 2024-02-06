fe66b3a34bd338c34e618b8d475c70865449336e
christian <christiany2026@student.cis.edu.hk>
2024-02-06 11:02:37 +0800

    speedrun

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,35 @@
 public class Address {
+    private String district;
+    private String street;
+    private int streetNumber;
+    public Address(String district, String street, int streetNumber){
+        this.district = district;
+        this.street = street;
+        this.streetNumber = streetNumber;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    public String getDistrict(){
+        return district;
+    }
+
+    public void setStreet(String setStreet){
+        this.street = street;
+    }
+    public String getStreet(){
+        return street;
+    }
+
+    public void setStreetNumber(int streetNumber){
+        this.streetNumber = streetNumber;
+    }
+    public int getStreetNumber(){
+        return streetNumber;
+    }
+
+    public String toString(){
+        return streetNumber + " " + street + ", " + district;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,68 @@
 public class Student {
+    private int age;
+    private String firstName;
+    private String lastName;
+    private int year;
+    private Address address;
+
+    public Student(int age, String firstName, String lastName, int year, Address address){
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.address = address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    private void addOneAge(){
+        age++;
+    }
+
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+
+    public String toString(){
+        String result = address.toString();
+        return firstName + " " + lastName + ", age " + age + ", year " + year + ", " + result;
+    }
+
+
 }
